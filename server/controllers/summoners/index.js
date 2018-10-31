07d4c91dd01d6a65ac34cda9e914ec2c7c6bab15
neilrichter <me@neilrichter.com>
2018-10-31 09:23:32 +0100

    Server does no longer download the file again if it already exists

--- server/controllers/summoners/index.js
+++ server/controllers/summoners/index.js
@@ -1,5 +1,6 @@
 const
   express = require('express'),
+  fs = require('fs'),
   { $LoL, $server } = require.main.require('./services'),
   app = express.Router();
 
@@ -14,7 +15,13 @@ app.get('/:summoner', (req, res, next) => {
 
 app.get('/:summoner/icon', async (req, res, next) => {
   const { data } = await $LoL.getSummonerIcon(req.params.summoner);
-  const url = await $server.download(data.iconUrl, 'public/img/profile_icons', `${data.iconId}`, 'png');
+  let url = '';
+  try {
+    fs.accessSync(`${process.cwd()}/public/img/profile_icons/${data.iconId}.png`);
+    url = `${process.cwd()}/public/img/profile_icons/${data.iconId}.png`
+  } catch(err) {
+    url = await $server.download(data.iconUrl, 'public/img/profile_icons', `${data.iconId}`, 'png');
+  }
   res.sendFile(url);
 });
 
